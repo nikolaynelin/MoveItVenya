bb60d18a2e9023b6b195dcad51ce0e1b25f2a75d
Veniamin Nelin <veniamin.nelin@gmail.com>
2018-04-04 21:10:47 +0300

    calculate price feature implemented

---
+++ MoveItVenya/MoveItVenya.Web/Controllers/PriceController.cs
@@ -0,0 +1,29 @@
+﻿using MoveItVenya.Logic;
+using System.Web.Mvc;
+
+namespace MoveItVenya.Web.Controllers
+{
+    public class PriceController : Controller
+    {
+        private IPriceService _priceService;
+
+
+        public PriceController()
+        {
+            _priceService = new PriceService();
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult CalculatePrice(double livingArea, double atticArea, double distance)
+        {
+            var result = _priceService.Calculate(livingArea, atticArea, distance);
+
+            return View("CalculatePriceResult", result);
+        }
+    }
+}
